c923b457a90f71c9af3c18409ac3f329897c79cc
Tomas Maly <tmaly1980@gmail.com>
2020-06-26 21:36:41 -0400

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf ios/common
+git clone https://github.com/mauron85/background-geolocation-ios.git ios/common
+rm -rf android/common
+git clone https://github.com/tmaly1980/background-geolocation-android.git android/common
